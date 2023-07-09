014f7e3c49846de6133b8ce41fb314cb5f75a3ad
homak666 <makajda1999@gmail.com>
2023-07-09 13:24:53 +0300

    Removed the db, major overhaul, added audio saving.

--- the_script.py
+++ the_script.py
@@ -16,22 +16,10 @@ import torch
 import whisper
 from pydub import AudioSegment
 from whisper.utils import get_writer
-import time
 
-MAX_RETRIES = 10
-RETRY_INTERVAL = 10
+#### STEP 1. CONNECT TO DATABASE AND GET LIST OF PODCASTS ####
 
-# function for cleaning up episode name
-
-def clean_title(title):
-    forbidden_symbols = r'[\u003c\u003e\u003a\u0022\u002f\u005c\u007c\u003f\*]'
-    cleaned_title = re.sub(forbidden_symbols, '', title)
-    cleaned_title = cleaned_title.replace(' ', '_')
-    return cleaned_title
-
-#### STEP 1. CONNECT TO DATABASE ####
-
-db_file = r"C:\Users\homak\Documents\GitHub\podscript\podcastindex_feeds.db"
+db_file = r"D:\podcastindex_feeds.db"
 transcripts_folder = r"D:\\Transcripts"
 
 if not os.path.exists(transcripts_folder):
@@ -40,62 +28,34 @@ if not os.path.exists(transcripts_folder):
 conn = sqlite3.connect(db_file)
 c = conn.cursor()
 
-#### STEP 2. GET THE NEW EPISODES IN AUDIO FORM ####
+c.execute("SELECT COUNT(*) FROM podcasts")
+row_count = c.fetchone()[0]
 
-# Select the URL and folder from the database
-c.execute("SELECT url, folder FROM podcasts")
-result = c.fetchall()
+def fetch_podcasts(cursor):
+    cursor.execute("SELECT url, folder FROM podcasts")
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        yield row
+
+#### STEP 2. PARSE THE LINK, ADD NEW EPISODES ####
 
 # Loop through each row in the result
-for row in result:
-    url = row[0]
-    folder = row[1]
+for i, (url, folder) in enumerate(fetch_podcasts(c), start=1):
     
-    # Make sure the folder exist and has the json in it
+    print(f"Now processing: {i}/{row_count} {folder}")
+    
+    # Make sure the folder exists
     try:
         folder_addr = os.path.join(transcripts_folder, folder)
         abs_path = os.path.abspath(folder_addr) #I made it absolute path because it broke once
         
         if not os.path.exists(abs_path):
             os.makedirs(abs_path)
+    except Exception as e:
+        print(f"Cannot create the folder:", e)      
         
-        open(os.path.join(abs_path, "feed.json"), "a")
-        print(f"Created folder for {folder}.")
-    except Exception as e:
-        print(f"Cannot create the folder or file for {folder}:", e)
-    
-    # Parse the json to get a list of existing episodes with download links
-    #TODO: make it into function, I literally copypaste this stuff twice
-
-    # Open the file
-    try:
-        json_name = os.path.join(folder_addr, "feed.json")
-        print(json_name)
-        with open(json_name, 'r', encoding='utf-8') as file:
-            content = file.read()
-        if not content.strip(): #if empty
-            existing_episodes = []
-        else:
-    # Parse the file
-            data = json.loads(content)
-            entries = data.get('episodes', [])
-            existing_episodes = []
-            for entry in entries:
-                episode = {}
-                episode['title'] = entry['title']
-                enclosures = entry.get('enclosures', [])
-                if len(enclosures) > 0:
-                    episode['url'] = enclosures[0]['url']
-                else:
-                    print("Enclosures array is empty for entry:", entry)
-                    continue
-                episode['published'] = entry['published']
-                existing_episodes.append(episode)
-    except Exception as e:
-        print(f'Could not open or parse the file {json_name}', e)
-    
-    # Give parser the link, get feed output in feed.json 
-    
     try:
         podcastparser.normalize_feed_url(url)
 
@@ -106,109 +66,178 @@ for row in result:
         parsed = podcastparser.parse(url, response)
         if not parsed:
             continue
-        output_file = json_name
-
-        with open(json_name, 'w', encoding='utf-8') as f:
-            json.dump(parsed, f, ensure_ascii=False, indent=4)
     except Exception as e:
         print(f'The parser could not parse the link {url}:', e)
+        continue         
+         
+    # Create a new database file in the folder
+    try:
+        db_path = os.path.join(abs_path, "feed.db")
+        conn2 = sqlite3.connect(db_path)
+        c2 = conn2.cursor()
+        
+        # Create a table with the required fields
+        c2.execute('''CREATE TABLE IF NOT EXISTS feed_data
+                        (title TEXT, published TIMESTAMP, url TEXT, duration TEXT, description TEXT, guid TEXT, script BOOLEAN DEFAULT 0, audio BOOLEAN DEFAULT 0)''')
+    except Exception as e:
+        print(f"An error occured while creating the db file:", e)
+        
+    # Insert the episode
+    for episode in parsed['episodes']:
+        enclosures = episode.get('enclosures')
+        if enclosures and len(enclosures) > 0:
+            ins_url = enclosures[0].get('url')
+        else:
+            continue
+        ins_title = episode['title']
+        ins_guid = episode.get('guid')
+        ins_published = episode.get('published')
+        ins_duration = episode.get('total_time', 0)
+        ins_description = episode.get('description')
+        
+        # Check if GUID exists
+        c2.execute("SELECT EXISTS (SELECT 1 FROM feed_data WHERE guid = ?)", (ins_guid,))
+        exists = c2.fetchone()[0]
+        
+        # If doesn't - insert into db file and write into missing_episodes
+        if not exists:
+            c2.execute("INSERT INTO feed_data (title, published, url, duration, description, guid) VALUES (?, ?, ?, ?, ?, ?)",
+                  (ins_title, ins_published, ins_url, ins_duration, ins_description, ins_guid))
+            
+    conn2.commit()
+    
+    # Find out how many episodes are missing script and audio
+    
+    # Count rows where script is 0
+    c2.execute("SELECT COUNT(*) FROM feed_data WHERE script = 0")
+    missing_script = c2.fetchone()[0]
+    
+    # Count rows where audio is 0
+    c2.execute("SELECT COUNT(*) FROM feed_data WHERE audio = 0")
+    missing_audio = c2.fetchone()[0]
+    
+    if missing_script == 0 and missing_audio == 0:
+        torch.cuda.empty_cache()
+        gc.collect()
+        os.system('cls' if os.name == 'nt' else 'clear')
         continue
     
-    # Parse the json to get a list of episodes with download links
+    c2.execute("SELECT COUNT(*) FROM feed_data WHERE script = 0 OR audio = 0")
+    missing_either = c2.fetchone()[0]
     
-    # Open the file
-    try:
-        with open(json_name, 'r', encoding='utf-8') as file:
-            content = file.read()
-        if not content.strip(): #skip if 0 episodes
-            print("Oopsie, 0 episoeds")
-            torch.cuda.empty_cache()
-            gc.collect()
+    # Load the db
+    c2.execute('SELECT * FROM feed_data')
+    rows = c2.fetchall()
+    
+    # Clean up title of the episode, used in the loop
+    def clean_title(title):
+        forbidden_symbols = r'[\u003c\u003e\u003a\u0022\u002f\u005c\u007c\u003f\u002A\u002e]'
+        cleaned_title = re.sub(forbidden_symbols, '', title)
+        cleaned_title = cleaned_title.replace(' ', '_')
+        return cleaned_title
+    
+#### STEP 3. LOOP THROUGH EPISODES, EXECUTE CODE BASED ON BOOLEN VALUES ####
+    
+    i2 = 1 #episode number, this is jank
+    # Start the loop of executing code based on boolean fields
+    for row in rows:
+        
+        title, published, url, duration, description, guid, script, audio = row
+        
+        # Establish the filepath
+        published_date = datetime.fromtimestamp(published).strftime('%Y%m%d')
+        cleaned_title = clean_title(title)
+        filename = f"{published_date}_{cleaned_title}"
+        filepath = os.path.join(folder_addr, filename)
+        
+        if script and audio or script is None:
             continue
-    except Exception as e:
-        print(f'Could not open the file {json_name}', e)
-    
-    # Parse the file
-    try:
-        data = json.loads(content)
-        entries = data.get('episodes', [])
-        episodes = []
-        for entry in entries:
-            episode = {}
-            episode['title'] = entry['title']
-            enclosures = entry.get('enclosures', [])
-            if len(enclosures) > 0:
-                episode['url'] = enclosures[0]['url']
-            else:
-                print("Enclosures array is empty for entry:", entry)
+        
+        print(f"Current episode: {i2}/{missing_either}", filename)
+        i2 = i2+1
+        
+        MAX_RETRIES = 1
+        RETRY_INTERVAL = 10
+        
+        # If all booleans are satisfied, skip, if not, download and convert audio
+        downloaded = False
+        for i in range(MAX_RETRIES):
+            try:
+                r = requests.get(url, timeout=60)
+                with open(filepath, 'wb') as f2:
+                    f2.write(r.content)
+                print(f"Downloaded the episode file.")
+                downloaded = True
+                break
+            except Exception as e:
+                print("An error occurred while downloading an episode:", e)
+                print(f"Retrying in {RETRY_INTERVAL} seconds...")
+                time.sleep(RETRY_INTERVAL)
                 continue
-            episode['published'] = entry['published']
-            episodes.append(episode)
-    except Exception as e:
-        print(f'Could not parse the file {json_name}', e)
-
-    # Compare the contents of feed.txt file with the episode list we gathered
-    missing_episodes = []
-    for episode in episodes:
-        if episode not in existing_episodes:
-            missing_episodes.append(episode)
-
-    # Download missing episodes and add them to feed.txt file
-    for episode in missing_episodes:
-        try:
-            for i in range(MAX_RETRIES):
-                try:
-                    r = requests.get(episode['url'], timeout=60)
-                    published_date = datetime.fromtimestamp(episode['published']).strftime('%Y%m%d')
-                    cleaned_title = clean_title(episode['title'])
-                    filename = f"{published_date}_{cleaned_title}"
-                    filepath = os.path.join(folder_addr, filename)
-                    with open(filepath, 'wb') as f2:
-                        f2.write(r.content)
-                    print(f"Downloaded {filename} for {folder}.")
-                    break
-                except Exception as e:
-                    print("An error occurred while downloading an episode:", e)
-                    print(f"Retrying in {RETRY_INTERVAL} seconds...")
-                    time.sleep(RETRY_INTERVAL)
-                    continue
-        except Exception as e:
-            print(f"Failed to download episode after {MAX_RETRIES} retries.")
-            gc.collect()
+        if not downloaded:
+            print("Download failed, skipping")
+            del downloaded
             continue
-            
-#### STEP 3. CONVERT AUDIO TO WAV (JUST IN CASE), THEN TO TEXT, DELETE AUDIO ####
 
-        # Convert audio file to wav if it's not already in wav format
         try:
             if not filepath.endswith('.wav'):
-                audio = AudioSegment.from_file(filepath)
+                wav_audio = AudioSegment.from_file(filepath)
                 new_filepath = os.path.splitext(filepath)[0] + '.wav'
-                audio.export(new_filepath, format='wav')
+                wav_audio.export(new_filepath, format='wav')
                 os.remove(filepath)
                 filepath = new_filepath
-                print(filepath)
+                print("Converted the episode file.")
         except Exception as e:
-            print ("An error converting the file:", e)
+            print("An error occured while converting the file:", e)
         
-        try:    
-            model = whisper.load_model("medium")
-            result = model.transcribe(filepath, verbose=False, temperature=0, compression_ratio_threshold=2.4, logprob_threshold=-1.0, no_speech_threshold=0.6, condition_on_previous_text=True, initial_prompt="Good day, AI. Please transcribe the following podcast recording with precise attention to grammar, syntax, and punctuation." )
-            srt_writer = get_writer("srt", folder_addr)
-            srt_writer(result, filepath)
-        except Exception as e:
-            print("An error occured while calling Whisper:", e)
-
+        # If the episode is not transcribed
+        if script is not None and not script:
+            try:    
+                model = whisper.load_model("medium")
+                result = model.transcribe(filepath, verbose=False, temperature=0, compression_ratio_threshold=2.4, logprob_threshold=-1.0, no_speech_threshold=0.6, condition_on_previous_text=True, initial_prompt="Transcribe the following podcast recording with precise attention to grammar, syntax, and punctuation." )
+                srt_writer = get_writer("srt", folder_addr)
+                srt_writer(result, filepath)
+                
+                print("Transcribed the episode.")
+                
+                # Change 'script' to 1 to mark it as transcribed
+                script = True
+                c2.execute('UPDATE feed_data SET script = ? WHERE guid = ?', (script, guid))
+                conn2.commit()
+            except Exception as e:
+                print("An error occured while calling Whisper:", e)
+        
+        # If episode's audio is not saved        
+        if audio is not None and not audio:
+            try:
+                episode_audio = AudioSegment.from_wav(filepath)
+                
+                mp3_path = os.path.splitext(filepath)[0] + '.mp3'
+                bitrate = "128k"
+                
+                episode_audio.export(mp3_path, format="mp3", bitrate=bitrate)
+                print("Saved the mp3 file.")
+                
+                # Change 'audio' to 1 to mark it as transcribed
+                audio = True
+                c2.execute('UPDATE feed_data SET audio = ? WHERE guid = ?', (audio, guid))
+                conn2.commit()
+            except Exception as e:
+                print(f"An error occured while saving mp3 file:", e)
+                
         # Delete audio file
         try:
             os.remove(filepath)
-            print(f"Deleted {filename} for {folder}.")
+            print(f"Deleted the episode file.")
         except Exception as e:
             print("An error occurred while deleting an audio file:", e)
         
-        # Garbage collection, not sure if needed but better safe then sorry
-        # TODO: figure out how garbage collection even works
-        del model
-        del result
-        torch.cuda.empty_cache()
-        gc.collect()
+        # Garbage collection
+        try:
+            del model
+            del result
+            torch.cuda.empty_cache()
+            gc.collect()
+            os.system('cls' if os.name == 'nt' else 'clear')
+        except Exception as e:
+            print("An error occured while garbage collecting:",e)
